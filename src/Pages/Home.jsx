544b49ea57139887d6290db8a514e86a75cbc244
ARIVAZHAGAN7B <barivazhagan7@gmail.com>
2025-02-04 14:33:19 +0530

    background added in home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
+import BackgroundImage from "../assets/background.webp";  // Corrected import
 
 const Home = () => {
   return (
-    <div>
-      <h1>This is Home</h1>
-    </div>
-  )
+    <>
+      <div style={{ 
+        backgroundImage: `url(${BackgroundImage})`, 
+        height: '100vh',  
+        backgroundSize: 'cover', 
+        backgroundPosition: 'center',
+        width:"99%",
+        margin:"6px",
+        borderRadius:"5px"
+      }}>
+      </div>
+    </>
+  );
 }
 
-export default Home
+export default Home;
